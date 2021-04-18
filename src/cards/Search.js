5bd5094fc89890e64f39efaeead19d27d6763248
sakertarsos <50601219+sakertarsos@users.noreply.github.com>
2021-04-17 18:01:18 -0700

    add collections search, search by tag

---
+++ src/cards/Search.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from "react";
+import Context from "../data/context";
+
+const Search = () => {
+  const [context, setContext] = useContext(Context);
+  const [data, setData] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let context2 = context;
+    context2.search = data;
+    setContext(() => {
+      return JSON.parse(JSON.stringify(context2));
+    });
+    console.log(context);
+  };
+
+  const resetSearch = () => {
+    let context2 = context;
+    context2.search = "none";
+    setContext(() => {
+      return JSON.parse(JSON.stringify(context2));
+    });
+    console.log(context);
+  };
+
+  return (
+    <h4 className="item">
+      <div className="row fullwidth name">
+        <form className="fullheight" onSubmit={handleSubmit}>
+          <input
+            className="button bordered marginleft"
+            placeholder="Enter tag or name"
+            type="text"
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+          />
+          <input
+            className="button bordered marginleft"
+            type="submit"
+            value="Search"
+          />
+        </form>
+        <button className="button bordered marginleft" onClick={resetSearch}>
+          Reset
+        </button>
+      </div>
+    </h4>
+  );
+};
+
+export default Search;
--- src/cards/Tag.js
+++ src/cards/Tag.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import Context from "../data/context";
 
 const Tag = (props) => {
-  return <span className="bit tag">{props.tag}</span>;
+  const [context, setContext] = useContext(Context);
+
+  const setTagSearch = () => {
+    let context2 = context;
+    context2.search = props.tag;
+    setContext(() => {
+      return JSON.parse(JSON.stringify(context2));
+    });
+  };
+
+  return (
+    <span className="bit button tag" onClick={setTagSearch}>
+      #{props.tag}
+    </span>
+  );
 };
 
 export default Tag;
