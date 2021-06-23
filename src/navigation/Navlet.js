bb1555ed255e065bc69a8cb9c453c262060a20ec
sakertarsos <50601219+sakertarsos@users.noreply.github.com>
2021-06-22 18:18:12 -0700

    svg icons for navlets and linklets creature card construction begins creature data template added

--- src/navigation/Navlet.js
+++ src/navigation/Navlet.js
@@ -1,8 +1,29 @@
 import React, { useContext } from "react";
 import Context from "../data/context.js";
+import { ReactComponent as Items } from "../data/icons/items.svg";
+import { ReactComponent as Skills } from "../data/icons/skills.svg";
+import { ReactComponent as Traits } from "../data/icons/traits.svg";
+import { ReactComponent as Spells } from "../data/icons/magic.svg";
+import { ReactComponent as Creatures } from "../data/icons/creatures.svg";
 
 const Navlet = (props) => {
   const [context, setContext] = useContext(Context);
+
+  const displayIcon = () => {
+    console.log(context.collections);
+    switch (props.navlet.name) {
+      case "items":
+        return <Items className="iconsvg" />;
+      case "skills":
+        return <Skills className="iconsvg" />;
+      case "traits":
+        return <Traits className="iconsvg" />;
+      case "spells":
+        return <Spells className="iconsvg" />;
+      case "creatures":
+        return <Creatures className="iconsvg" />;
+    }
+  };
 
   return (
     <button
@@ -21,7 +42,10 @@ const Navlet = (props) => {
         })
       }
     >
-      {props.navlet.display}
+      {displayIcon()}
+      {context.collections === props.navlet.name && (
+        <span className="navletSpan">{props.navlet.display}</span>
+      )}
     </button>
   );
 };
