53385e618403afa2b5cd23e59af31ab3a1a380c0
sakertarsos <50601219+sakertarsos@users.noreply.github.com>
2021-11-18 22:01:34 -0800

    decode updates

--- src/utils/decode.js
+++ src/utils/decode.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import Character from "../data/character.js";
 import WorldState from "../data/worldstate.js";
 import Context from "../data/context";
+import orders from "../data/orders.json";
 
 //add toaster notifications
 
@@ -12,6 +13,20 @@ const decode = (code) => {
 
   const set = (content, method) => {
     method(JSON.parse(JSON.stringify(content)));
+  };
+
+  const mod = (type, stat, amount) => {
+    if (type === "char") {
+      let newCharacter = character;
+      newCharacter[stat] += amount;
+      set(newCharacter, setCharacter());
+    }
+    if (typeof type === "number") {
+      let placement = type;
+      let newWorldState = worldState;
+      newWorldState.creatures[placement][stat] += amount;
+      set(newWorldState, setWorldState());
+    }
   };
 
   const damage = (subject) => {
@@ -54,17 +69,51 @@ const decode = (code) => {
     }
   };
 
+  const add = (type, category, card) => {
+    if (type === "char") {
+      if (!character[category]) return;
+      if (!orders[category][card]) return;
+      let newCharacter = character;
+      newCharacter[category].push(card);
+      set(newCharacter, setCharacter());
+    }
+    if (type === "world") {
+      if (!worldState[category]) return;
+      if (!orders[category][card]) return;
+      let newWorldState = worldState;
+      newWorldState[category].push(card);
+      set(newWorldState, setWorldState());
+    }
+  };
+
   let lineArray = code.split(", ");
   for (line of lineArray) {
+    if (line.includes(":")) {
+      //line is conditional
+      //if conditional is untrue, return;
+    }
+
     let wordArray = line.split(" ");
     let action = wordArray.shift();
 
     switch (action) {
+      case "mod":
+        mod(wordArray[0], wordArray[1], wordArray[2]);
+        break;
+      case "xp":
+        mod("char", "xp", wordArray[0]);
+        break;
+      case "cash":
+        mod("char", "cash", wordArray[0]);
+        break;
       case "damage":
         damage(wordArray);
         break;
       case "heal":
         heal(wordArray);
+        break;
+      case "add":
+        add(wordArray[0], wordArray[1], wordArray[2]);
         break;
       default:
         return;
