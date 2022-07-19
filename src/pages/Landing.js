1a59a62e87069b2586d20ecebfc9d710ade43c9f
Matthew Bozin <mbozin94@gmail.com>
2022-07-18 22:19:17 -0700

    Removed Top Cat Toolkit and Rules sections from landing page.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import Context from "../data/context";
 import LandingOption from "../components/bits/LandingOption";
-import { ReactComponent as Title} from "../data/icons/title.svg";
+import { ReactComponent as Title } from "../data/icons/title.svg";
 import Col from "react-bootstrap/esm/Col";
 import Row from "react-bootstrap/esm/Row";
 
@@ -18,26 +18,46 @@ const Landing = () => {
     let newContext = context;
     newContext.persona = newPersona;
     setContext(JSON.parse(JSON.stringify(newContext)));
-  }
+  };
 
   let image = require("../data/landing.png");
 
   return (
     <div className="outerbox">
-        <Row>
-            <Col>
-                <Title className="landingtitlesvg mleft5px"/>
-                <span className="heading ptop50px">v0.9.9 [beta]</span>
-                <LandingOption name={"COLLECTIONS"} page={"collections"} method={changePage} text={"CONTENT CARD DATABASE"} />
-                <LandingOption name={"HERO SHEET"} page={"sheet"} method={() => {changePage("sheet"); changePersona("PC");}} text={"INTERACTIVE CHARACTER SHEET"} />
-                <LandingOption name={"TOP CAT TOOLKIT"} page={"sheet"} method={() => {changePage("sheet"); changePersona("TC");}} text={"UNDER CONSTRUCTION"} />
-                <LandingOption name={"RULES"} text={"UNDER CONSTRUCTION"} />
-                <LandingOption name={"ABOUT"} page={"about"} method={changePage} text={"CREDITS AND OTHER INFO"} />
-            </Col>
-            <img className="none992 mright12px" src={image} alt={"Seek the blessings of the great machine."}></img>
-        </Row>
+      <Row>
+        <Col>
+          <Title className="landingtitlesvg mleft5px" />
+          <span className="heading ptop50px">v0.9.9 [beta]</span>
+          <LandingOption
+            name={"COLLECTIONS"}
+            page={"collections"}
+            method={changePage}
+            text={"CONTENT CARD DATABASE"}
+          />
+          <LandingOption
+            name={"HERO SHEET"}
+            page={"sheet"}
+            method={() => {
+              changePage("sheet");
+              changePersona("PC");
+            }}
+            text={"INTERACTIVE CHARACTER SHEET"}
+          />
+          <LandingOption
+            name={"ABOUT"}
+            page={"about"}
+            method={changePage}
+            text={"CREDITS AND OTHER INFO"}
+          />
+        </Col>
+        <img
+          className="none992 mright12px"
+          src={image}
+          alt={"Seek the blessings of the great machine."}
+        ></img>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
